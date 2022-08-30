8855f67683e3bd2d239c5e197f27f93937af09f8
gompanda <browoo051@Gmail.com>
2022-08-30 11:57:07 +0900

    220803 자바스크립트 공부
    산술연잔자 단항연산자

---
+++ arithmetic산술연산자.js
@@ -0,0 +1,21 @@
+// 산술 연잔사 (Arithemetic operators)
+// +
+// -
+// * 곱하기
+// /
+// % 나머지값
+// ** 지수연산자 (거듭제곱)
+
+console.log(5 + 2);
+console.log(5 - 2);
+console.log(5 * 2);
+console.log(5 / 2);
+console.log(5 ** 2); // es7에서 추가됨
+console.log(Math.pow(5, 2)); // 원래 이걸썻음
+
+// + 연산자 주의점!
+let text = '두개의' + '문자를';
+console.log(text);
+
+text = '1' + 1; //숫자와 문자열을 더하면 문자열로 변환됨
+console.log(text);
